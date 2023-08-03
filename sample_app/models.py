469dff00aa4b3306c2c55ad8d4d4d5c7e48a2f26
taku820801 <takuchanpower@gmail.com>
2023-08-03 18:24:56 +0900

    コミット

--- sample_app/models.py
+++ sample_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=5000)
+    tags = models.ManyToManyField(Tag, related_name='tags', blank=True)
+
+    def __str__(self):
+        return self.title
--- sample_app/views.py
+++ sample_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Blog
 
-# Create your views here.
+
+class BlogView(TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        # IDを指定して一つ取り出し
+        # blog = Blog.objects.get(id=1)
+        # all_tags = blog.tags.all()
+
+        # タグのIDを指定して全件取り出し
+        blog = Blog.objects.prefetch_related('tags').get(id=2)
+
+        # これは動かない
+        blog = Blog.objects.select_related('title').get(id=2)
+
+
+        return render(request, 'index.html', {
+            'blog': blog,
+            # 'all_tags': all_tags
+        })
+
+# https://www.northtorch.co.jp/archives/1151
+# prefetch_related()とselect_related()
